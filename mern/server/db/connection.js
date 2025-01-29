5ee03435be941bb53b1a9bc23bdfce90a6ea68d5
restjohnson <149099469+restjohnson@users.noreply.github.com>
2025-01-29 18:48:16 -0500

    Makes changes to the server files

--- mern/server/db/connection.js
+++ mern/server/db/connection.js
@@ -1,6 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const URI = process.env.ATLAS_URI || "";
+const URI = "mongodb+srv://Team8112:Team8112@cluster0.kpa5d.mongodb.net/employees?retryWrites=true&w=majority";
 const client = new MongoClient(URI, {
   serverApi: {
     version: ServerApiVersion.v1,
